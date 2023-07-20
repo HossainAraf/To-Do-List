eceb6732ac86474c2f3c25a23a0006ba6e31112d
Hossain <arafat.kd99@gmail.com>
2023-07-20 14:08:50 +0100

    Add list object

--- src/index.js
+++ src/index.js
@@ -3,3 +3,21 @@ import twitter from './img/twitter.svg';
 
 const twit = document.querySelector('#im');
 twit.src = twitter;
+
+const listTask = [
+  {
+    index: 0,
+    description: 'desc-1',
+    completed: true,
+  },
+  {
+    index: 1,
+    description: 'desc-2',
+    completed: true,
+  },
+  {
+    index: 2,
+    description: 'desc-3',
+    completed: true,
+  },
+];
