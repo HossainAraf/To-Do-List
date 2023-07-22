184246948b5aae293f70caf114b5cd9b0a2a8a1f
Hossain <arafat.kd99@gmail.com>
2023-07-22 18:31:07 +0100

    Add Render function

--- src/display.js
+++ src/display.js
@@ -21,3 +21,48 @@ document.getElementById('wrapper').appendChild(formContainer);
 const form = document.createElement('form');
 form.id = 'form';
 document.getElementById('form-container').appendChild(form);
+
+
+const formInput = document.createElement('input');
+formInput.id = 'formInput';
+formInput.type = 'text';
+formInput.placeholder = 'Add to your list...';
+formInput.value = '';
+document.getElementById('form').appendChild(formInput);
+
+const formButton = document.createElement('button');
+formButton.id = 'formButton';
+formButton.type = 'submit';
+document.getElementById('form').appendChild(formButton);
+
+const arrowIcon = new Image();
+arrowIcon.src = arrow;
+document.getElementById('formButton').appendChild(arrowIcon);
+
+const todoList = document.createElement('form');
+todoList.id = 'todo';
+todoList.className = 'todo';
+document.getElementById('wrapper').appendChild(todoList);
+
+const clear = document.createElement('div');
+clear.id = 'clear';
+clear.innerHTML = 'Clear all completed!';
+document.getElementById('wrapper').appendChild(clear);
+
+// Render todo list
+export default function renderTodoList() {
+  const todoListContainer = document.getElementById('todo');
+  todoListContainer.innerHTML = '';
+  tasks.sort((a, b) => a.index - b.index);
+  tasks.forEach((task) => { // Update id when rendering the todo list
+    const listItem = document.createElement('div');
+    listItem.className = 'todo-item';
+    listItem.innerHTML = `
+      <div class="checkbox"><i class="fa-regular fa-square"></i></div>
+      <div class="description" contenteditable="true">${task.description}</div>
+      <div id="delete" class="delete"><i class="fa fa-trash" aria-hidden="true"></i></div>
+      <div class="elipsis"><i class="fa-solid fa-ellipsis-vertical"></i></div>
+    `;
+    todoListContainer.appendChild(listItem);
+  });
+}
